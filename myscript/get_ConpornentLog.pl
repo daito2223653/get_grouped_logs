5d03009f66c393df8b5a4c2e26beda88e45f55bc
Daito Keigo <daitokeigo@daito-dev-3.novalocal>
2019-09-03 17:50:41 +0900

    add get_compornent_log.pl

---
+++ myscript/get_ConpornentLog.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/env perl
+
+# written by daito 
+# this script is to get logs from sw360chore's Container.
+
+# how to use ***
+# ./get_conpornentlog.pl [compornent of sw360chores] [local file copied from log]  [option]
+# option is
+# -t : with timestamp
+#
+
+
+
+
